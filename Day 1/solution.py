af08a754178e59598368ff76db40c2ef3f1de885
Xavier Hamel <xavierhamel02+git@gmail.com>
2024-12-01 10:35:51 -0500

    Day 1 challenge 2

--- Day 1/solution.py
+++ Day 1/solution.py
@@ -14,7 +14,36 @@ def parse(path) -> (list[int], list[int]):
 
     return list1, list2
 
+#Challenge 1
+def distance(list1, list2):
 
+    total = 0
+
+    for i in range(len(list1)):
+        dist_value = abs(list1[i] - list2[i])
+        total += dist_value
+
+    return total
+
+def create_value_count_dict(values:list) -> dict[int, int]:
+    value_count_dict = {}
+
+    for value in values:
+        value_count_dict[value] = value_count_dict.get(value, 0) + 1
+
+    return value_count_dict
+
+#Challenge 2
+def similarity(list1, list2):
+    value_count_dict1 = create_value_count_dict(list1)
+    value_count_dict2 = create_value_count_dict(list2)
+
+    similarity_score = 0
+
+    for key, value in value_count_dict1.items():
+        similarity_score += key * value * value_count_dict2.get(key, 0)
+
+    return similarity_score
 
 
 def main():
@@ -23,15 +52,8 @@ def main():
     list1.sort()
     list2.sort()
 
-    total = 0
-
-    for i in range(len(list1)):
-        dist_value = abs(list1[i] - list2[i])
-        total += dist_value
-
-    print(total)
-
-
+    print(f'Distance(Challenge 1): {distance(list1, list2)}')
+    print(f'Similarity(Challenge 2): {similarity(list1, list2)}')
 
 
 if __name__ == '__main__':
