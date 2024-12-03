c47f452e6c385a40d9d0e799fca61f74a2e5d12f
Xavier Hamel <xavierhamel02+git@gmail.com>
2024-12-03 11:36:04 -0500

    Day 2 challenge 2

--- Day 2/solution.py
+++ Day 2/solution.py
@@ -1,9 +1,7 @@
-import sys
 
 
 def parse(path) ->  list[list[int]]:
     data = []
-
     with open(path) as file:
         for line in file:
             numbers = line.strip().split(' ')
@@ -12,45 +10,53 @@ def parse(path) ->  list[list[int]]:
                 for number in numbers:
                     number = int(number)
                     level.append(number)
-
                 data.append(level)
-
     return data
 
 def is_adjacent_diff_safe(e1:int, e2:int) ->bool:
     diff = abs(e1 - e2)
-
-    if 1 <= diff <= 3:
-        return True
-
-    return False
+    return 1 <= diff <= 3
 
 def is_increasing(e1:int, e2:int) ->bool:
     return e1 < e2
 
-def check_level(level: list[int]) -> bool:
+def list_without_index(l: list[int], index) -> list[int]:
+    return [e for i, e in enumerate(l) if i != index]
+
+def check_level_basic(level: list[int]) -> bool:
 
     is_level_increasing = is_increasing(level[0], level[1])
+    is_safe_enough = True
 
     for i in range(len(level)-1):
-        if is_level_increasing != is_increasing(level[i], level[i+1]): return False
-        if not is_adjacent_diff_safe(level[i], level[i+1]): return False
+        if is_level_increasing != is_increasing(level[i], level[i+1]): is_safe_enough = False
+        if not is_adjacent_diff_safe(level[i], level[i+1]): is_safe_enough = False
 
-    return True
+    return is_safe_enough
 
-#Challenge 1
-def safe_report_count(levels):
+
+def check_level_tolerate_error(level: list[int], max_error = 0, error_count = 0) -> bool:
+    if check_level_basic(level): return True
+    if max_error == error_count: return False
+
+    for i in range(len(level)):
+        if check_level_tolerate_error(list_without_index(level, i), max_error, error_count + 1) : return True
+
+    return False
+
+#Challenge 1 and 2
+def safe_report_count(reports, tolerated_unsafe_levels):
     total = 0
-
-    for level in levels:
-        if check_level(level): total += 1
+    for report in reports:
+        if check_level_tolerate_error(report, tolerated_unsafe_levels): total += 1
     return total
 
 
 def main():
     reports = parse('./input.txt')
 
-    print(f'Safe Reports (Challenge 1): {safe_report_count(reports)}')
+    print(f'Safe Reports (Challenge 1): {safe_report_count(reports, 0)}')
+    print(f'Safe Reports (Challenge 2): {safe_report_count(reports, 1)}')
 
 
 if __name__ == '__main__':
